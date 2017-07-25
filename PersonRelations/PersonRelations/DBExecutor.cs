4690b6ef0ad9f2eaf5b08729f4ff654865ba3e1a
Faizal P.A <faizal@getrealhealth.com>
2017-07-25 14:52:46 +0530

    First Commit

---
+++ PersonRelations/PersonRelations/DBExecutor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace InventoryManagement
+{
+    public class DBExecutor<T>
+    {
+        public delegate T Exec(SqlCommand myConn);
+        public T Execute(Exec exeObj)
+        {
+            string MyConnection2 = WebConfigurationManager.AppSettings["ConnectionString"];
+            SqlConnection myConn=myConn = new SqlConnection(MyConnection2);
+            myConn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = myConn;
+            T ret=exeObj.Invoke(cmd);
+            myConn.Close();
+            return ret;
+        }
+    }
+}
